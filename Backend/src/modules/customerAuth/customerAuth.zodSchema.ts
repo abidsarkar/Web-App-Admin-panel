56059b22bce66b25359381f2e291194f8976fb68
abidsarkar <abidsarkar0@gmail.com>
2025-11-10 20:58:23 +0600

    revoke change of customer auth zod as mistake copy pest

--- Backend/src/modules/customerAuth/customerAuth.zodSchema.ts
+++ Backend/src/modules/customerAuth/customerAuth.zodSchema.ts
@@ -1,27 +1,79 @@
-// src/modules/cart/cart.zodSchema.ts
-import { z } from "zod";
+import { email, z } from "zod";
 
-export const addToCartSchema = z.object({
-  productId: z.string({ message: "Product ID is required" }).trim().min(1),
-  quantity: z.number({ message: "Quantity is required" }).min(1).max(100),
-  size: z.string().trim().optional(),
-  color: z.string().trim().optional(),
-  colorCode: z.string().trim().optional(),
+export const passwordSchema = z
+  .string({ message: "Password is required" })
+  .min(6, { message: "Password must be at least 6 characters long" })
+  .max(30, { message: "Password must be at most 30 characters long" })
+  .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/, {
+    message: "Password must contain both letters and numbers",
+  });
+export const loginSchema = z.object({
+  email: z.string({ message: "Invalid email address!" }).trim().email(),
+  password: passwordSchema,
+});
+export const addressSchema = z.object({
+  division: z.string().max(30).trim().optional(),
+  district: z.string().max(30).trim().optional(),
+  upazila: z.string().max(30).trim().optional(),
+  village_road_house_flat: z.string().max(200).trim(),
+});
+//register new customer schema
+export const registerNewCustomerSchema = z
+  .object({
+    firstName: z
+      .string({ message: "name is required" })
+      .min(1, { message: "Name cannot be empty" })
+      .max(25, { message: "name must be under 50 character" }),
+    lastName: z
+      .string({ message: "name is required" })
+      .min(1, { message: "Name cannot be empty" })
+      .max(25, { message: "name must be under 50 character" })
+      .optional(),
+    email: z.string({ message: "email is required" }).trim().email(),
+    phone: z
+      .string()
+      .min(1, { message: "phone number cannot be empty" })
+      .max(18, {
+        message:
+          "to much number character for phone number use a valid phone number",
+      }),
+    address: addressSchema,
+    password: passwordSchema,
+    confirmPassword: passwordSchema,
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "passwords do not match",
+    path: ["confirmPassword"],
+  });
+//export type LoginInput = z.infer<typeof loginSchema>["body"];
+export const emailSchema = z.object({
+  email: z.string({ message: "Invalid email address!" }).trim().email(),
+});
+export const otpSchema = z.object({
+  otp: z
+    .string({ message: "otp must be send in string format and non empty" })
+    .regex(/^\d{6}$/, { message: "OTP must be exactly 6 digits number" }),
+  email: z.string({ message: "Invalid email address!" }).trim().email(),
 });
 
-export const updateCartItemSchema = z.object({
-  productId: z.string({ message: "Product ID is required" }).trim().min(1),
-  quantity: z.number({ message: "Quantity is required" }).min(0).max(100),
-  size: z.string().trim().optional(),
-  color: z.string().trim().optional(),
-});
-
-export const removeFromCartSchema = z.object({
-  productId: z.string({ message: "Product ID is required" }).trim().min(1),
-  size: z.string().trim().optional(),
-  color: z.string().trim().optional(),
-});
-
-export const mergeCartsSchema = z.object({
-  sessionId: z.string({ message: "Session ID is required" }).trim().min(1),
-});
+export const changePasswordSchema = z
+  .object({
+    email: z.string({ message: "Invalid email address!" }).trim().email(),
+    password: passwordSchema,
+    confirmPassword: passwordSchema,
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"], // highlight confirmPassword field
+  });
+export const changePasswordFromProfileSchema = z
+  .object({
+    _id: z.string().trim().min(2).max(100),
+    currentPassword: passwordSchema,
+    newPassword: passwordSchema,
+    confirmPassword: passwordSchema,
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"], // highlight confirmPassword field
+  });
