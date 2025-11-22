41706f9bda3207e4a246b7bb2087eda945866595
abidsarkar <abidsarkar0@gmail.com>
2025-11-22 22:52:53 +0600

    docker compose for redis

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -13,7 +13,7 @@ import { template } from "./rootTemplate";
 import { globalRateLimiter } from "./middlewares/rateLimiter";
 import mongoSanitizer, { SanitizedRequest } from 'mongo-sanitizer';
 import { FRONTEND_URL } from "./config/envConfig";
-import favicon from "serve-favicon"
+//import favicon from "serve-favicon"
 // Create an Express application
 const app: Application = express();
 //for global no sql injection prevent
